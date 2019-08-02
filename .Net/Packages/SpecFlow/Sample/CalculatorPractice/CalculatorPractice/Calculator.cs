d40f750a6d629e98fba11e99b78a6ae334e39553
Kuei <ragnakuei@livemail.tw>
2019-08-02 23:36:46 +0800

    add specflow note

---
+++ .Net/Packages/SpecFlow/Sample/CalculatorPractice/CalculatorPractice/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorPractice
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
